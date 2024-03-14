a588384e8e0e7cb2b278449636fb428e60eee32b
dhakshnamoorthy2703 <132809268+dhakshnamoorthy2703@users.noreply.github.com>
2024-03-15 02:41:56 +0530

    Add files via upload

---
+++ Resources/javascript/script.js
@@ -0,0 +1,21 @@
+const sr = ScrollReveal ({
+    distance: '40px',
+    duration: 2500,
+    reset: true
+})
+
+sr.reveal('.home-img',{delay:800, origin:'bottom'});
+sr.reveal('.logo',{delay:200, origin:'left'});
+sr.reveal('.navbar',{delay:200, origin:'top'});
+sr.reveal('.home-text span',{delay:280, origin:'top'});
+sr.reveal('.home-text h1',{delay:200, origin:'left'});
+sr.reveal('.home-text p',{delay:200, origin:'right'});
+sr.reveal('.main-btn',{delay:600, origin:'left'});
+sr.reveal('.card-main-btn',{delay:200, origin:'bottom'});
+sr.reveal('.share p',{delay:600, origin:'left'});
+sr.reveal('.social',{delay:600, origin:'right'});
+sr.reveal('.slider',{delay:100, origin:'left'});
+sr.reveal('.card',{delay:100, origin:'left'});
+sr.reveal('.card1',{delay:100, origin:'left'});
+sr.reveal('.card3',{delay:100, origin:'right'});
+sr.reveal('.card2',{delay:100, origin:'bottom'});
